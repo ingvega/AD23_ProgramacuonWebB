e0505f0043a665fc59c8b19b4c7b113e1307467a
ingvega <isc.p.vega@gmail.com>
2023-09-27 19:15:03 -0600

    Uso básico de variables y funciones

---
+++ UsoVariablesYFunciones/funciones.js
@@ -0,0 +1,12 @@
+alert('Ejecución de mensaje al cargar el script');
+var x=5;
+let y='Hola';
+const z=true;
+a=20.5;
+alert(x+a);
+
+function saludar(){
+    var nombre=document.getElementById("txtNombre").value;
+    alert('Hola' + nombre);
+
+}
