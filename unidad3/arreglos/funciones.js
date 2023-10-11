1e583c5b7c546294fadbf1a727d8ddd21b8e3ef7
ingvega <isc.p.vega@gmail.com>
2023-10-11 15:54:42 -0600

    Uso de utilerías de JSON y localstorage

--- unidad3/arreglos/funciones.js
+++ unidad3/arreglos/funciones.js
@@ -36,5 +36,43 @@ obj3['nombre']='Juan perez';
 obj3['clave']=1;
 obj3['semestre']=1;
 
-arr2.push(obj,obj2,obj3);
+arr2.push(obj,obj2);//,obj3);
+
+let empleado={clave:1,
+            nombre:'Juan Perez',
+            sueldo:3000,
+            dependientes:[
+                {nombre:'Alberto Perez',parentezco:'hijo'},
+                {nombre:'Maria Juarez',parentezco:'esposa'}
+            ]};
+
+let listaEmpleados=[];
+listaEmpleados.push(empleado);
+empleado={clave:2,
+    nombre:'Luis Martinez',
+    sueldo:5000,
+    dependientes:[
+        {nombre:'Gerardo Martinez',parentezco:'hijo'},
+        {nombre:'Laura Juarez',parentezco:'esposa'}
+    ]};
+listaEmpleados.push(empleado);
+console.log(JSON.stringify(listaEmpleados));
+
+
+//localStorage.setItem(clave,valor)
+localStorage.setItem(1,JSON.stringify(obj));
+localStorage.setItem('empleados',JSON.stringify(listaEmpleados));
 debugger;
+localStorage.setItem(1,'hola');
+let empleadosCadena=localStorage.getItem('empleados');
+let texto=localStorage.getItem(1);
+
+console.log(empleadosCadena);
+
+let arregloEmpleados=JSON.parse(empleadosCadena);
+console.log(arregloEmpleados);
+debugger;
+
+let contenedor=document.querySelector('#contenedor');
+//contenedor.innerHTML='<b>texto en negrita</b>';
+contenedor.innerText='<b>texto en negrita</b>';
