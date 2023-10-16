6fccd390ae5629110ceba23fde05c2338eb76dff
ingvega <isc.p.vega@gmail.com>
2023-10-16 16:25:06 -0600

    Catálogo funcionando con Agregar y Editar

--- unidad3/arreglos/tabla.js
+++ unidad3/arreglos/tabla.js
@@ -36,21 +36,30 @@ document.addEventListener('DOMContentLoaded',()=>{
                 personas=JSON.parse(localStorage.getItem('personas'));
             }
             let operacion=document.querySelector("form h4").innerText;
+            let clave=0;
             if(operacion=='Agregar'){
                 //Llenar un objeto con los elementos de la caja de texto
-                let clave=0;
                 if(personas.length>0){
                     clave=personas[personas.length-1].clave;
                 }          
                 clave++;      
                 let nuevaPersona={
                     clave:clave,
-                    nombre:document.getElementById("txtNombre").value,
+                    nombre:document.getElementById("txtNombre").value.trim(),
                     edad:document.getElementById("txtEdad").value
                 };
                 personas.push(nuevaPersona);
                 localStorage.setItem('personas',JSON.stringify(personas));
                 alert('Registro añadido');
+            }else if(operacion=='Editar'){
+                clave=document.getElementById("txtClave").value;
+                let index=personas.findIndex(item=>item.clave==clave);
+                personas[index].nombre=document.getElementById("txtNombre").value.trim();
+                personas[index].edad=document.getElementById("txtEdad").value;
+                localStorage.setItem('personas',JSON.stringify(personas));
+                alert('Registro almacenado');
+                //if('1'==1)->true
+                //if('1'===1)->false
             }
         }
     });
@@ -62,18 +71,66 @@ function llenarTabla2(datos){
     datos.forEach(p => {
         fila=document.createElement('tr');
         let celda=document.createElement('td');
-        celda=document.createElement('td');
         celda.innerText=p.clave;
         fila.appendChild(celda);
+        
         celda=document.createElement('td');
         celda.innerText=p.nombre;
         fila.appendChild(celda);
+        
         celda=document.createElement('td');
         celda.innerText=p.edad;
         fila.appendChild(celda);
+
+        let btnEditar=document.createElement('button');
+        btnEditar.innerText='Editar';
+        btnEditar.addEventListener('click',()=>{
+            //Cargar la información almacenada
+            let personas=localStorage.getItem('personas')?
+            JSON.parse(localStorage.getItem('personas')):[];
+            //Buscar la persona que tenga la clave de la fila
+            
+            let persona=personas.find(item=>item.clave==p.clave);
+            if(persona){
+                document.getElementById("txtClave").value=persona.clave;
+                document.getElementById("txtNombre").value=persona.nombre;
+                document.getElementById("txtEdad").value=persona.edad;
+                document.querySelector("form h4").innerText='Editar';
+            }else{
+                alert('Registro no encontrado');
+            }
+        });
+
+        let btnEliminar=document.createElement('button');
+        btnEliminar.innerText='Eliminar';
+        debugger;
+        btnEliminar.onclick='eliminar('+p.clave+')';
+        
+        celda=document.createElement('td');
+        celda.appendChild(btnEditar);
+        celda.appendChild(btnEliminar);
+        fila.appendChild(celda);
+
         tbody.appendChild(fila);
     });
     
+}
+
+function eliminar(clave){
+     //Cargar la información almacenada
+     let personas=localStorage.getItem('personas')?
+     JSON.parse(localStorage.getItem('personas')):[];
+     //Buscar la persona que tenga la clave de la fila
+     
+     let persona=personas.find(item=>item.clave==p.clave);
+     if(persona){
+         document.getElementById("txtClave").value=persona.clave;
+         document.getElementById("txtNombre").value=persona.nombre;
+         document.getElementById("txtEdad").value=persona.edad;
+         document.querySelector("form h4").innerText='Editar';
+     }else{
+         alert('Registro no encontrado');
+     }
 }
 
 function llenarTabla(datos){
@@ -82,7 +139,7 @@ function llenarTabla(datos){
         fila='<tr><td>'+p.clave+'</td><td>'+p.nombre+
         '</td><td>'+p.edad+'</td></tr>';
         
-        fila=`<tr><td>${p.clave}</td><td>${p.nombre}</td><td>${p.edad}</td></tr>`;
+        fila=`<tr><td${p.clave}</td><td>${p.nombre}</td><td>${p.edad}</td></tr>`;
         cadena+=fila;
     });
     document.querySelector("#listaPersonas tbody").innerHTML=cadena;
