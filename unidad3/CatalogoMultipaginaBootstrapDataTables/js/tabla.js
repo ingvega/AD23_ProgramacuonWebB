72b2f9598d23ca370da1df2f8d0b0bad0d899763
ingvega <isc.p.vega@gmail.com>
2023-11-04 08:23:21 -0600

    Correcciones

--- unidad3/CatalogoMultipaginaBootstrapDataTables/js/tabla.js
+++ unidad3/CatalogoMultipaginaBootstrapDataTables/js/tabla.js
@@ -98,7 +98,43 @@ function llenarTabla2(datos){
         tbody.appendChild(fila);
     });
 
-    new DataTable('#listaPersonas');
+    //new DataTable("#listaPersonas");
+    $("#listaPersonas").DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'pageLength',
+            {
+                extend: 'copyHtml5',
+                exportOptions: {
+                    columns: [0,1,2]
+                }
+            },
+            {
+                extend: 'print',
+                exportOptions: {
+                    columns: [0,1,2]
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: [0,1,2]
+                }
+            },
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: [0,1,2]
+                }
+            },
+            'colvis'
+        ],
+        stateSave: true,
+        columnDefs: [
+            { orderable: false, targets: -1 }
+        ],
+        order: [[1, 'asc'],[2,'desc']]
+    });
 
     
 }
@@ -116,9 +152,9 @@ function eliminar(){
             location.reload()}
             );*/
     }else{
-        alert('registro no encontrado');
+        mostrarMensaje('Registro no encontrado','danger',()=>location.reload());    
     }
-    llenarTabla2(personas);
+    
 
 }
 
