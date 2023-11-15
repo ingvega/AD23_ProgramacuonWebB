427a8a7413a5f7b23c874ecece97c46956de3b60
ingvega <isc.p.vega@gmail.com>
2023-11-15 15:05:59 -0600

    Validación

--- unidad4/MiApp/usuario.php
+++ unidad4/MiApp/usuario.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
     <?php
@@ -15,9 +16,19 @@
     ?>
     <pre>
     <?php
-      var_dump($_GET);
-      print_r($_POST);
+      //echo //Imprimir lo que sea en el html (cadenas)
+      /*var_dump($_GET);
+      print_r($_POST);*/
       var_dump($_REQUEST);
+     function marcar($interes){
+        if(ISSET($_POST["Intereses"])){
+            for($i=0;$i<count($_POST["Intereses"]);$i++){
+                if($_POST["Intereses"][$i]==$interes)
+                    return "checked";
+            }
+        }
+        return "";
+     }
     ?>
     </pre>
     <form method="post">
@@ -26,42 +37,49 @@
         value="<?php echo ISSET($_POST["Nombre"])?$_POST["Nombre"]:"" ?>" required>
         </p>
         <p>
-            <input type="text" id="txtApellido1" name="Apellido1" value="<?= ISSET($_POST["Apellido1"])?$_POST["Apellido1"]:"" ?>" placeholder="Primer apellido" required>
+            <input type="text" id="txtApellido1" name="Apellido1" 
+            value="<?= ISSET($_POST["Apellido1"])?$_POST["Apellido1"]:"" ?>" placeholder="Primer apellido" required>
         </p>
         <p>
             <input type="text" id="txtApellido2" name="Apellido2" placeholder="Segundo apellido">
+        </p>
+        <p>
+            <input type="email" name="Email" id="">
         </p>
         <p>
         <input type="number" id="txtEdad" name="Edad" placeholder="Edad" required>
         </p>
         <p>
         <label>
-            <input type="radio" id="rbtMasculino" name="Genero" value="Masculino">
+            <input type="radio" id="rbtMasculino" name="Genero"
+             value="Masculino" checked>
             Masculino
         </label>
 
         <label>
-            <input type="radio" id="rbtFemenino" name="Genero" Value="Femenino">
+            <input type="radio" id="rbtFemenino" name="Genero" 
+            Value="Femenino"
+            <?= ISSET($_POST["Genero"])?($_POST["Genero"]=="Femenino"?"checked":""):"" ?>>
             Femenino
         </label>
         </p>
         <p>
         <h4>Intereses</h4>
         <label>
-            <input type="checkbox" name="Intereses[]" value="Tecnologia"> Tecnología
+            <input type="checkbox" name="Intereses[]" value="Tecnologia" <?= marcar("Tecnologia")?>> Tecnología
         </label>
         <label>
-            <input type="checkbox" name="Intereses[]" value="Cambio climático"> Cambio climático
+            <input type="checkbox" name="Intereses[]" value="Cambio climático" <?= marcar("Cambio climático")?>> Cambio climático
         </label>
         <label>
-            <input type="checkbox" name="Intereses[]" value="Covid19"> COVID-19
+            <input type="checkbox" name="Intereses[]" value="Covid19" <?= marcar("Covid19")?>> COVID-19
         </label>
         <label>
-            <input type="checkbox" name="Intereses[]" value="Politica"> Política
+            <input type="checkbox" name="Intereses[]" value="Politica" <?= marcar("Politica")?>> Política
         </label>
         </p>
         <p>
-            <select name="EstadoCivil">
+            <select multiple name="EstadoCivil[]">
                 <option value="1">Soltero</option>
                 <option value="2">Casado</option>
                 <option value="3">Divorciado</option>
@@ -76,6 +94,7 @@
         </p>
         
         <button>Enviar</button>
+        <script src="js/bootstrap.bundle.min.js"></script>
     </form>
 </body>
-</html>>
+</html>
